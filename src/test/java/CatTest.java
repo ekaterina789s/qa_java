69b00d3edd00fb40166ad9f4685089699f555fe5
Ekaterina Maslova <ekaterinamas@yandex.com>
2026-05-16 18:15:25 +0300

    "В методе с аннотацией @Before лежит инициализация объекта Cat"

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,6 +1,7 @@
 import com.example.Cat;
 import com.example.Feline;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -14,21 +15,23 @@ public class CatTest {
     @Mock
     Feline feline;
 
+    private Cat cat;
+
+    @Before
+    public void setUp() {
+        cat = new Cat(feline);
+    }
+
     //Проверка метода getSound() класса Cat
     @Test
     public void getSoundTest(){
-        Cat cat = new Cat(feline);
         Assert.assertEquals("Мяу", cat.getSound());
     }
     //Проверка метода getFood() класса Cat
     @Test
     public void getFoodCatTest() throws Exception{
-        Cat cat = new Cat(feline);
         Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), cat.getFood());
 
     }
-
-
-
 }
